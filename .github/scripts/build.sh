1a502579835ee71a54519af579c6f6dbca3c56f3
hardware-crash <hardware.crash@gmail.com>
2024-11-11 23:49:52 +0100

    Update build.sh

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -5,20 +5,14 @@ ARCH_INPUT=$1
 
 # Architektur-Mapping
 case "$ARCH_INPUT" in
-    amd64)
+    x86_64 | amd64)
         ARCH="x86_64"
         ;;
-    armhf)
+    armv6l | armhf)
         ARCH="armv6l"
         ;;
     aarch64)
         ARCH="aarch64"
-        ;;
-    x86_64)
-        ARCH="x86_64"
-        ;;
-    armv6l)
-        ARCH="armv6l"
         ;;
     *)
         echo "Unsupported architecture: $ARCH_INPUT"
@@ -36,11 +30,13 @@ FILE_URL="${DOWNLOAD_URL}/v${BUILD_VERSION}/HyperHDR-${BUILD_VERSION}-Linux-${AR
 echo "Downloading ${FILE_URL}"
 
 # Download und Extraktion
-wget "${FILE_URL}" -O /tmp/HyperHDR.tar.gz
+wget --tries=3 "${FILE_URL}" -O /tmp/HyperHDR.tar.gz
 
 # Überprüfe die heruntergeladene Datei
 echo "Verifiziere die heruntergeladene Datei:"
 ls -lh /tmp/HyperHDR.tar.gz
 
-# Extrahiere nach /usr
+# Extrahiere nach /usr mit angepassten Berechtigungen
 tar -xvz --no-same-owner --no-same-permissions -C /usr -f /tmp/HyperHDR.tar.gz
+
+echo "Extraction complete."
