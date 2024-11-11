786bb3e44652c07cfc5e5ea8d2ab995b25cc4ee5
hardware-crash <hardware.crash@gmail.com>
2024-11-11 23:41:08 +0100

    Update build.sh

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -1,13 +1,46 @@
-#!/bin/sh
+#!/bin/bash
 set -e
-ARCH=$1
 
-docker run --rm --privileged \
-    -v /var/run/docker.sock:/var/run/docker.sock:ro \
-    -v ${GITHUB_WORKSPACE:-$(PWD)}/addon-hyperhdr:/data \
-    homeassistant/amd64-builder \
-    --target /data \
-    --docker-user "${DOCKER_USER}" \
-    --docker-password "${DOCKER_PASSWORD}" \
-    --no-latest \
-    --${ARCH:-all}
+ARCH_INPUT=$1
+
+# Architektur-Mapping
+case "$ARCH_INPUT" in
+    amd64)
+        ARCH="x86_64"
+        ;;
+    armhf)
+        ARCH="armv6l"
+        ;;
+    aarch64)
+        ARCH="aarch64"
+        ;;
+    x86_64)
+        ARCH="x86_64"
+        ;;
+    armv6l)
+        ARCH="armv6l"
+        ;;
+    *)
+        echo "Unsupported architecture: $ARCH_INPUT"
+        exit 1
+        ;;
+esac
+
+# Definiere Download-URL und Build-Version
+DOWNLOAD_URL="https://github.com/awawa-dev/HyperHDR/releases/download"
+BUILD_VERSION="${RELEASE}" # Annahme: RELEASE ist in der Umgebung gesetzt
+
+# Konstruiere die Datei-URL
+FILE_URL="${DOWNLOAD_URL}/v${BUILD_VERSION}/HyperHDR-${BUILD_VERSION}-Linux-${ARCH}.tar.gz"
+
+echo "Downloading ${FILE_URL}"
+
+# Download und Extraktion
+wget "${FILE_URL}" -O /tmp/HyperHDR.tar.gz
+
+# Überprüfe die heruntergeladene Datei
+echo "Verifiziere die heruntergeladene Datei:"
+ls -lh /tmp/HyperHDR.tar.gz
+
+# Extrahiere nach /usr
+tar -xvz -C /usr -f /tmp/HyperHDR.tar.gz
