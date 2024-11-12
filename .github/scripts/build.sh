c43a1ebb1dd0fcc7e41c208d23a67722335ee706
hardware-crash <hardware.crash@gmail.com>
2024-11-12 10:52:53 +0100

    Update build.sh

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -15,8 +15,8 @@ case "$ARCH" in
         ARCH_FLAG="--amd64"
         ;;
     armv6l | armhf)
-        BUILDER_IMAGE="homeassistant/armv7-builder"
-        ARCH_FLAG="--armv7"
+        BUILDER_IMAGE="homeassistant/armhf-builder"
+        ARCH_FLAG="--armhf"
         ;;
     aarch64)
         BUILDER_IMAGE="homeassistant/aarch64-builder"
